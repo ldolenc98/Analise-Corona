438384b70bdd71c7f73da9c4588c2b08d0deb35a
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-04-14 18:44:14 -0300

    Update Corona.py

--- Corona.py
+++ Corona.py
@@ -5,38 +5,26 @@ import re
 
 data = datetime.now()
 
-data1 = data.strftime("%d/%m/%Y %H:%M:%S")
+data1 = data.strftime("%Y/%m/%d %H:%M:%S")
 
 if sys.argv[1:] == []:
-
-    r = requests.get('https://www.worldometers.info/coronavirus/')
-
-    texto = r.text
     
-    padrao_casos = re.findall(r'aaa\">(\d+,\d+,\d+)\s<', texto)
-    casos = padrao_casos[0]
-
-    padrao_mortes = re.findall(r'<span>(\d+,\d+)</span>', texto)
-    mortes = padrao_mortes[0]
-
-    padrao_recuperados = re.findall(r'<span>(\d+,\d+)</span>', texto)
-    recuperados = padrao_recuperados[1]
-
-    print(casos + " ; " + mortes + " ; " + recuperados + " ; " + data1)
-
+    pais = ""
+       
 else:
     
-    r = requests.get('https://www.worldometers.info/coronavirus/country/{}/'.format(sys.argv[1]))
+    pais = "/country/%s/" % sys.argv[1]
     
-    texto = r.text
+r = requests.get('https://www.worldometers.info/coronavirus'+ pais)
+
+texto = r.text
+
+padrao_casos = re.findall(r'\">(.*?.*?.*?.*?\d+)\s<', texto)
+casos = padrao_casos[0]
+
+padrao_mortes_recuperados = re.findall(r'<span>(.*?.*?.*?.*?\d+)</span>', texto)
+mortes = padrao_mortes_recuperados[0]
+recuperados = padrao_mortes_recuperados[1]
     
-    padrao_casos = re.findall(r'aaa\">(.*?.*?.*?.*?\d+)\s<', texto)
-    casos = padrao_casos[0]
-
-    padrao_mortes = re.findall(r'<span>(.*?.*?.*?.*?\d+)</span>', texto)
-    mortes = padrao_mortes[0]
-
-    padrao_recuperados = re.findall(r'<span>(.*?.*?.*?.*?\d+)</span>', texto)
-    recuperados = padrao_recuperados[1]
-
-    print(casos + " ; " + mortes + " ; " + recuperados + " ; " + data1)
+print(casos + " ; " + mortes + " ; " + recuperados + " " + data1)
+    
