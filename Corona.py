1a620be42e060d13105615aa967d543ecf27376c
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-04-15 19:23:22 -0300

    Update Corona.py

--- Corona.py
+++ Corona.py
@@ -19,12 +19,14 @@ r = requests.get('https://www.worldometers.info/coronavirus'+ pais)
 
 texto = r.text
 
-padrao_casos = re.findall(r'\">(.*?.*?.*?.*?\d+)\s<', texto)
+padrao_casos = re.findall(r'\">(.*\d+,\d+)\s<', texto)
 casos = padrao_casos[0]
 
-padrao_mortes_recuperados = re.findall(r'<span>(.*?.*?.*?.*?\d+)</span>', texto)
+padrao_mortes_recuperados = re.findall(r'<span>(.*.*\d+)</span>', texto)
 mortes = padrao_mortes_recuperados[0]
 recuperados = padrao_mortes_recuperados[1]
-    
+
+      
 print(casos + " ; " + mortes + " ; " + recuperados + " " + data1)
+    
     
